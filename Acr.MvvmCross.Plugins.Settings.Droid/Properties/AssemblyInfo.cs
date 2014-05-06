6c9cf7f6f520d7eded960af18e9fc1b12ec3c229
Allan Ritchie <allan.ritchie@gmail.com>
2014-05-06 11:32:23 -0400

    Updating versions in settings Removing windows store barcode lib

--- Acr.MvvmCross.Plugins.Settings.Droid/Properties/AssemblyInfo.cs
+++ Acr.MvvmCross.Plugins.Settings.Droid/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
 ﻿using System.Reflection;
-using Android.App;
 
 [assembly: AssemblyTitle("Acr.MvvmCross.Plugins.Settings.Droid")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-
-//[assembly: UsesPermission(Android.Manifest.Permission.WriteSettings)]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
