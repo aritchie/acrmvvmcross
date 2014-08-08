8a59771b50c5e5c04ab1f2bf1e6888cc4439ad86
Allan Ritchie <allan.ritchie@gmail.com>
2014-08-08 08:41:46 -0400

    Added winphone project and nuspec fixes

--- nuspec/pack.cmd
+++ nuspec/pack.cmd
@@ -1,6 +1,7 @@
 @echo off
 del *.nupkg
 nuget pack Acr.MvvmCross.nuspec
+nuget pack Acr.MvvmCross.Plugins.FileSystem.nuspec
 nuget pack Acr.MvvmCross.Plugins.BarCodeScanner.nuspec
 nuget pack Acr.MvvmCross.Plugins.DeviceInfo.nuspec
 nuget pack Acr.MvvmCross.Plugins.Settings.nuspec
