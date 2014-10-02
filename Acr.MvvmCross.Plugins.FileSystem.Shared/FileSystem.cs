94f6580cdef440c22533917df5616e14275b166a
Allan Ritchie <allan.ritchie@gmail.com>
2014-10-02 09:37:04 -0400

    Update to iOS8 file structure Shared projects are a pain in the ass

--- Acr.MvvmCross.Plugins.FileSystem.Shared/FileSystem.cs
+++ Acr.MvvmCross.Plugins.FileSystem.Shared/FileSystem.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
-
+#if __IOS__
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+#endif
 
 namespace Acr.MvvmCross.Plugins.FileSystem {
 
@@ -14,13 +17,16 @@ namespace Acr.MvvmCross.Plugins.FileSystem {
             this.Public = new Directory(Path.Combine(path, "Public"));
             this.Temp = new Directory(Path.Combine(path, "Temp"));
 #elif __IOS__
-            var documents = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var documents = UIDevice.CurrentDevice.CheckSystemVersion(8, 0)
+                ? NSFileManager.DefaultManager.GetUrls (NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomain.User)[0].Path
+                : Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
             var library = Path.Combine(documents, "..", "Library");
             this.AppData = new Directory(library);
             this.Cache = new Directory(Path.Combine (library, "Caches"));
             this.Temp = new Directory(Path.Combine(documents, "..", "tmp"));
             this.Public = new Directory(documents);
-#elif __ANDROID__            
+#elif __ANDROID__
             this.AppData = new Directory(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
             this.Cache = new Directory(Android.App.Application.Context.CacheDir.AbsolutePath);
             this.Temp = new Directory(Android.App.Application.Context.CacheDir.AbsolutePath);
