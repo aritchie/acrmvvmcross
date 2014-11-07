7c6925da5ae448d09d67625d931cdc5ed6561595
Allan Ritchie <allan.ritchie@gmail.com>
2014-11-07 08:43:04 -0500

    Nuspec updates

---
+++ nuspec/push.cmd
@@ -0,0 +1,12 @@
+@echo off
+del *.nupkg
+nuget push Acr.MvvmCross.Plugins.FileSystem.nuspec
+nuget push Acr.MvvmCross.Plugins.DeviceInfo.nuspec
+nuget push Acr.MvvmCross.Plugins.Settings.nuspec
+nuget push Acr.MvvmCross.Plugins.UserDialogs.nuspec
+nuget push Acr.MvvmCross.Plugins.Network.nuspec
+
+rem nuget push Acr.MvvmCross.nuspec
+rem nuget push Acr.MvvmCross.Plugins.BarCodeScanner.nuspec
+rem nuget push Acr.MvvmCross.Plugins.SignaturePad.nuspec
+pause
